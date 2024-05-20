3cd44e4c931f131f0aaf0bdf14f6e963e2cc40ac
Semhylmz <semh-ylmz@hotmail.com>
2024-05-21 00:26:04 +0300

    ble on off control, scan ble devices

--- flutter_ble_app/lib/views/home_view.dart
+++ flutter_ble_app/lib/views/home_view.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_ble_app/views/scan_view.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +12,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late bool _isScanning;
+
+  late BluetoothConnectionState _bluetoothConnectionState;
+
+  late List<ScanResult> _scanResults;
+  late StreamSubscription<List<ScanResult>> _scanResultsSubscription;
+  late StreamSubscription<bool> _isScanningSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _scanResults = [];
+    _isScanning = false;
+
+    _bluetoothConnectionState = BluetoothConnectionState.disconnected;
+    _scanResultsSubscription = FlutterBluePlus.scanResults.listen(
+      (results) {
+        _scanResults = results;
+        if (mounted) setState(() {});
+      },
+      onError: (e) => print('Error scan result subscription: $e'),
+    );
+
+    _isScanningSubscription = FlutterBluePlus.isScanning.listen((state) {
+      if (mounted) setState(() => _isScanning = state);
+    });
+
+    scanDevice();
+  }
+
+  @override
+  void dispose() {
+    _scanResultsSubscription.cancel();
+    _isScanningSubscription.cancel();
+    super.dispose();
+  }
+
+  Future scanDevice() async {
+    _scanResults.clear();
+
+    try {
+      int divisor = Platform.isAndroid ? 8 : 1;
+      await FlutterBluePlus.startScan(
+        timeout: const Duration(seconds: 15),
+        continuousUpdates: true,
+        continuousDivisor: divisor,
+      );
+    } catch (e) {
+      print('Error scan device method: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text('Home Page'),
+      body: Center(
+        child:
+            _bluetoothConnectionState == BluetoothConnectionState.disconnected
+                ? ScanPage(
+                    scanResults: _scanResults,
+                    connect: (p0) {},
+                    isScanning: _isScanning,
+                    timerValue: 15,
+                  )
+                : const SizedBox.shrink(),
       ),
     );
   }
--- flutter_ble_app/lib/views/my_app_view.dart
+++ flutter_ble_app/lib/views/my_app_view.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_ble_app/views/bluetooth_off_view.dart';
 import 'package:flutter_ble_app/views/home_view.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -9,11 +12,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late BluetoothAdapterState _bluetoothAdapterState;
+  late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
+
+  @override
+  void initState() {
+    _bluetoothAdapterState = BluetoothAdapterState.unknown;
+    _adapterStateStateSubscription =
+        FlutterBluePlus.adapterState.listen((state) {
+      _bluetoothAdapterState = state;
+      if (mounted) setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _adapterStateStateSubscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    Widget currentPage = _bluetoothAdapterState == BluetoothAdapterState.on
+        ? const HomePage()
+        : const BleOffPage();
+
+    return MaterialApp(
       title: 'Flutter BLE App',
-      home: HomePage(),
+      home: currentPage,
     );
   }
 }
